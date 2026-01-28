1648d124b73a58ab1fac9e762bc544f912b9c504
berkeklc <berkeklc.123@gmail.com>
2026-01-28 20:21:58 +0300

    Remove production password reset bootstrap script

--- apps/cms/src/index.ts
+++ apps/cms/src/index.ts
@@ -16,25 +16,5 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap({ strapi }: { strapi: any }) {
-    console.log('--- Production Password Reset Bootstrap ---');
-    try {
-      const user = await strapi.db.query('admin::user').findOne({
-        where: { email: 'berkeklc.123@gmail.com' },
-      });
-
-      if (user) {
-        console.log('Found admin user, resetting password...');
-        await strapi.service('admin::user').updateById(user.id, {
-          password: 'RushZanzibar2026!',
-        });
-        console.log('SUCCESS: Password reset for production.');
-      } else {
-        console.log('User berkeklc.123@gmail.com not found in database.');
-      }
-    } catch (error) {
-      console.error('FAILED: Password reset bootstrap error:', error);
-    }
-    console.log('------------------------------------------');
-  },
+  bootstrap(/* { strapi }: { strapi: any } */) { },
 };
