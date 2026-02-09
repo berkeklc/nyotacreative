2d20773b450cd3c310351ae4aa8b2de58b0fcdfe
berkeklc <berkeklc.123@gmail.com>
2026-02-09 06:13:21 +0300

    fix(travel): Hardcode GTM ID to resolve undefined env var issue

--- apps/travel/app/layout.tsx
+++ apps/travel/app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+              })(window,document,'script','dataLayer','GTM-MLXVCHP5');
             `,
           }}
         />
@@ -105,7 +105,7 @@ export default function RootLayout({
       <body>
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MLXVCHP5"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
